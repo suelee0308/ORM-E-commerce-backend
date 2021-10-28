1c0c2b5b608d7d305f59337690ca0853ddf8151f
Sue Lee <sue.lee0308@gmail.com>
2021-10-28 10:00:07 -0500

    models done

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,11 +6,27 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll();
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    Category.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [Product]
+    }).then((response) => res.json(response))
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
